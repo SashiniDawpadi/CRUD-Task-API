43d2797cf92b988f661243bbdf9c75880fac1b62
Sashini Dawpadi <86653321+SashiniDawpadi@users.noreply.github.com>
2022-12-17 09:56:57 +0530

    updated models (task | taskList)

--- app.js
+++ app.js
@@ -1,8 +1,45 @@
-const express = require('express');
+const { request, response } = require("express");
+const express = require("express");
 const app = express();
+const mongoose = require("./database/mongoose");
 
-const mongoose = require('./database/mongoose')
+const TaskList = require("./database/models/taskList");
+const Task = require("./database/models/task");
+
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Pass to next layer of middleware
+  next();
+});
+
+app.use(express.json()); // or body parser
+
+//Routes
+app.get("/taskLists", (req, res) => {
+  TaskList.find({})
+    .then((lists) => {
+      res.send(lists);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
 
 app.listen(3000, () => {
-    console.log("Server listening on port 3000");
+  console.log("Server listening on port 3000");
 });
